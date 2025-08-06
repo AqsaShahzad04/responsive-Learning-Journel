5ff642e0f16f4eaec1591221e0929920e6e6ab91
aqsa <aqsa70729@gmail.com>
2025-08-07 01:01:39 +0500

    fixed the bug

--- scripts/data.js
+++ scripts/data.js
@@ -1,41 +1,41 @@
 export const blogs = [
     {
-        image: '../public/images/img-1.png',
+        image: '/images/img-1.png',
         date: 'july 22,2024',
         heading: 'Blog one',
         blogText:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     }
     ,
     {
-        image: '../public/images/img-2.png',
+        image: '/images/img-2.png',
         date: 'july 24,2024',
         heading: 'Blog Two',
         blogText:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     }
     ,
     {
-        image: '../public/images/img-3.png',
+        image: '/images/img-3.png',
         date: 'Aug 22,2024',
         heading: 'Blog Three',
         blogText:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     }
     ,
     {
-        image: '../public/images/img-4.png',
+        image: '/images/img-4.png',
         date: 'jan 22,2025',
         heading: 'Blog Four',
         blogText:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     }
     ,
     {
-        image: '../public/images/img-5.png',
+        image: '/images/img-5.png',
         date: 'july 30,2025',
         heading: 'Blog Five',
         blogText:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     }
     ,
     {
-        image: '../public/images/img-6.png',
+        image: '/images/img-6.png',
         date: 'dec 18,2025',
         heading: 'Blog Six',
         blogText:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
